91a42ba481786ba30cf425c7e4695ffed3f74bea
LilianMwaura <wambuimwaura28@gmail.com>
2022-04-29 18:33:16 +0300

    add classes to config.py

--- app/views.py
+++ app/views.py
@@ -1,3 +1,4 @@
+from turtle import title
 from flask import render_template
 from app import app
 
@@ -8,4 +9,11 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html')
+    title = "NewsFlash - Latest breaking news and information on top stories"
+    return render_template('index.html',title=title)
+@app.route('/article/<id>')
+def article(id):
+    
+    
+    articles = get_article(id)
+    return render_template('article.html',articles=articles,id=id )
