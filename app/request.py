0ef51b8e205c1744b949edfa91a3e9f8ab8d9e72
LilianMwaura <wambuimwaura28@gmail.com>
2022-05-03 08:05:09 +0300

    create request gor get articles

--- app/request.py
+++ app/request.py
@@ -1,6 +1,7 @@
 from app import app
 import urllib.request,json
 from .models import articles
+from .models import source
 
 Articles= articles.Articles
 # Getting api key
@@ -9,45 +10,74 @@ api_key = app.config['ARTICLES_API_KEY']
 # Getting the articles base url
 base_url = app.config["ARTICLES_API_BASE_URL"]
 
-def get_articles(category):
+def get_sources():
+    """
+    A function that gets the json files from our url request
+    """
+    get_sources_url = base_url.format(api_key)
+
+    with urllib.request.urlopen(get_sources_url) as url:
+        get_sources_data = url.read()
+        get_sources_response = json.loads(get_sources_data)
+
+        source_results= None
+
+        if get_sources_response["sources"]:
+            source_results_list = get_sources_response['sources']
+            source_results = process_results(source_results_list)
+
+    return source_results
+
+def process_results(source_list):
+    """
+    A function that processes the news sources results
+   
+    """
+    source_results=[]
+    for source_item in source_list:
+        id = source_item.get("id")
+        name = source_item.get("name")
+        description = source_item.get("description")
+        url = source_item.get("url")
+        category = source_item.get("category")
+
+
+        source_object=source.Source(id,name,description,url,category)
+        source_results.append(source_object)
+
+    return source_results
+
+def get_article(id):
+    article_source_url = 'https://newsapi.org/v2/top-headlines?sources={}&api_key={}'.format(id,api_key)
+    print(article_source_url)
+    with urllib.request.urlopen(article_source_url) as url:
+        article_source_data = url.read()
+        article_source_response = json.loads(article_source_data)
+
+        article_source_results = None
+
+        if article_source_response['articles']:
+            article_source_list = article_source_response['articles']
+            article_source_results = process_articles_results(article_source_list)
+
+
+    return article_source_results
+
+def process_articles_results(news):
     '''
-    Function that gets the json response to our url request
+    A function that processes the json files of news articles from api
     '''
-    get_articles_url = base_url.format(category,api_key)
+    article_source_results = []
+    for article in news:
+        author = article.get('author')
+        title = article.get('title')
+        description = article.get('description')
+        url = article.get('url')
+        urlToImage = article.get('urlToImage')
+        publishedAt = article.get ('publishedAt')
 
-    with urllib.request.urlopen(get_articles_url) as url:
-        get_articles_data = url.read()
-        get_articles_response = json.loads(get_articles_data)
+        if url:
+            article_objects = articles.Article(author,title,description,url,urlToImage,publishedAt)
+            article_source_results.append(article_objects)
 
-        articles_results = None
-
-        if get_articles_response['results']:
-            articles_results_list = get_articles_response['results']
-            articles_results = process_results(articles_results_list)
-
-
-    return articles_results
-
-def process_results(articles_list):
-    '''
-    Function  that processes the articles result and transform them to a list of Objects
-
-    Args:
-        articles_list: A list of dictionaries that contain movie details
-
-    Returns :
-        articles_results: A list of movie objects
-    '''
-    articles_results = []
-    for articles_item in articles_list:
-        id = articles_item.get("id")
-        name = articles_item.get("name")
-        description = articles_item.get("description")
-        url = articles_item.get("url")
-        category = articles_item.get("category")
-
-        
-        articles_object = Articles(id,name,description,url,category)
-        articles_results.append(articles_object)
-
-    return articles_results    
+    return article_source_results
